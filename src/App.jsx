3becc3259e48655b0f12dd18dbad4f141c270c46
Angad Jangir <aiajajaiintelligence@gmail.com>
2026-06-30 09:35:30 +0530

    feat: login page with Google OAuth + IITM domain gating

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import LoginPage from './pages/LoginPage'
 export default function App() {
-  return <div className="p-8 font-display text-4xl text-maroon">IITM Pulse</div>
+  return <LoginPage />
 }
