6823af1ce74a416ad7fe5f93a9f4a2c61ba83012
Angad Jangir <aiajajaiintelligence@gmail.com>
2026-06-30 09:26:15 +0530

    chore: scaffold React + Vite + Tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        maroon: {
+          DEFAULT: '#8B0000',
+          light: '#B22222',
+          50: '#FFF5F5',
+          100: '#FFE0E0',
+        },
+      },
+      fontFamily: {
+        display: ['"Space Grotesk"', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
